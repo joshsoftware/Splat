571c1d5ce14fee56c6d55e486690c755291585cc
Jiren <jiren@Jiren.local>
2010-08-23 13:07:37 +0530

    Test code

--- test.rb
+++ test.rb
@@ -2,33 +2,37 @@
 require 'rubygems'
 require 'lib/splat'
 
-number = '+91 9960054954'
+number = '+91 9850888082'
 
 gateways = {}
-gateways['bulksms'] = Splat::Base.new(:bulksmspune)
-gateways['vmobo'] = Splat::Base.new(:vmobo)
+#gateways['bulksms'] = Splat::Base.new(:bulksmspune)
+#gateways['vmobo'] = Splat::Base.new(:vmobo)
+gateways['clickatell'] = Splat::Base.new(:clickatell)
 
 
+=begin
+#number = '+1 5599726538'
+number = '+91 9881395656'
 msg = "Testing for splat"
 gateways.each do |provider, obj|
   p "Sending SMS via #{provider}"
   p obj.send_sms(msg, number)
   p "SMS Sent successfully via #{provider}"
 end
-
 p "========================"
 
 msg = "Testing for splat"
-numbers = ['+91 9960054954', '+91 9960054954']
+numbers = ['+91 9960054954', '+91 9850888082']
 gateways.each do |provider, obj|
   p obj.send_bulk_sms(msg, numbers) 
   p "Bulk Send successfully via #{provider}"
 end
 p "========================"
 
+=end
 custom_msg = "Hi $1, hold on to your $2."
-options = {'+91 9970831117' => ['Sagar', 'coffee'],
-           '+91 9860352988' => ['Gautam' , 'stocks']}
+options = {'+91 9960054954' => ['Sagar', 'coffee'],
+           '+91 9850888082' => ['Gautam' , 'stocks']}
            #'+91 9880397111' => ['Jane' , 'skirt']}
 
 gateways.each do |provider, obj|
